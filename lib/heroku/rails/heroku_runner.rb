0a971934a5422ea5cd45df42f1687bd27187bb31
Jacques Crocker <railsjedi@gmail.com>
2010-11-01 12:41:50 -0700

    Adding the rest of the setup tasks to heroku runner

--- lib/heroku/rails/heroku_runner.rb
+++ lib/heroku/rails/heroku_runner.rb
@@ -95,8 +95,103 @@ module Heroku
           collaborator_emails.each do |collaborator_email|
             # check to see if we need to add this person
             unless existing_emails.include?(collaborator_email)
-              # add the collaborator if they are on the approved list
+              # add the collaborator if they are not already on the server
               system_with_echo "heroku sharing:add #{collaborator_email} --app #{app_name}"
+            end
+          end
+        end
+      end
+
+      # setup configuration
+      def setup_config
+        authorize unless @heroku
+        each_heroku_app do |heroku_env, app_name, repo|
+          # get the configuration that we are aiming towards
+          new_config = @config.config(heroku_env)
+
+          # get the existing config from heroku's servers
+          existing_config = @heroku.config_vars(app_name) || {}
+
+          # find the config variables to add
+          add_config = {}
+          new_config.each do |new_key, new_val|
+            add_config[new_key] = new_val unless existing_config[new_key] == new_val
+          end
+
+          # persist the changes onto heroku
+          unless add_config.empty?
+            # add the config
+            set_config = ""
+            add_config.each do |key, val|
+              set_config << "#{key}='#{val}' "
+            end
+
+            system_with_echo "heroku config:add #{set_config} --app #{app_name}"
+          end
+        end
+      end
+
+      # setup the addons for heroku
+      def setup_addons
+        authorize unless @heroku
+        each_heroku_app do |heroku_env, app_name, repo|
+          # get the addons that we are aiming towards
+          addons = @config.addons(heroku_env)
+
+          # add "custom_domains" if that addon doesnt already exist
+          # and we have domains configured for this app
+          unless @config.domains(heroku_env).empty? or addons.include?("custom_domains")
+            addons << "custom_domains"
+          end
+
+          # get the addons that are already on the servers
+          existing_addons = (@heroku.installed_addons(app_name) || []).map{|a| a["name"]}
+
+          # remove the addons that need to be removed
+          existing_addons.each do |existing_addon|
+            # check to see if we need to delete this addon
+            unless addons.include?(existing_addon)
+              # delete this addon if they arent on the approved list
+              system_with_echo "heroku addons:remove #{existing_addon} --app #{app_name}"
+            end
+          end
+
+          # add the addons that dont exist already
+          addons.each do |addon|
+            # check to see if we need to add this addon
+            unless existing_addons.include?(addon)
+              # add this addon if they are not already added
+              system_with_echo "heroku addons:add #{addon} --app #{app_name}"
+            end
+          end
+        end
+      end
+
+      # setup the domains for heroku
+      def setup_domains
+        authorize unless @heroku
+        each_heroku_app do |heroku_env, app_name, repo|
+          # get the domains that we are aiming towards
+          domains = @config.domains(heroku_env)
+
+          # get the domains that are already on the servers
+          existing_domains = (@heroku.list_domains(app_name) || []).map{|a| a[:domain]}
+
+          # remove the domains that need to be removed
+          existing_domains.each do |existing_domain|
+            # check to see if we need to delete this domain
+            unless domains.include?(existing_domain)
+              # delete this domain if they arent on the approved list
+              system_with_echo "heroku domains:remove #{existing_domain} --app #{app_name}"
+            end
+          end
+
+          # add the domains that dont exist already
+          domains.each do |domain|
+            # check to see if we need to add this domain
+            unless existing_domains.include?(domain)
+              # add this domain if they are not already added
+              system_with_echo "heroku domains:add #{domain} --app #{app_name}"
             end
           end
         end
--- spec/heroku/rails/heroku_config_spec.rb
+++ spec/heroku/rails/heroku_config_spec.rb
@@ -3,51 +3,51 @@ require 'spec_helper'
 module Heroku::Rails
   describe HerokuConfig do
     before(:each) do
-      @heroku = HerokuConfig.new(config_path("heroku-config.yml"))
+      @config = HerokuConfig.new(config_path("heroku-config.yml"))
     end
 
     it "should read the configuration file" do
-      @heroku.settings.should_not be_empty
+      @config.settings.should_not be_empty
     end
 
     describe "#apps" do
       it "should return the list of apps defined" do
-        @heroku.apps.should have(2).apps
-        @heroku.apps.should include("production" => "awesomeapp")
-        @heroku.apps.should include("staging" => "awesomeapp-staging")
+        @config.apps.should have(2).apps
+        @config.apps.should include("production" => "awesomeapp")
+        @config.apps.should include("staging" => "awesomeapp-staging")
       end
     end
 
     describe "#app_names" do
       it "should return the list of apps defined" do
-        @heroku.app_names.should have(2).names
-        @heroku.app_names.should include("awesomeapp")
-        @heroku.app_names.should include("awesomeapp-staging")
+        @config.app_names.should have(2).names
+        @config.app_names.should include("awesomeapp")
+        @config.app_names.should include("awesomeapp-staging")
       end
     end
 
     describe "#app_environments" do
       it "should return a list of the environments defined" do
-        @heroku.app_environments.should have(2).environments
-        @heroku.app_environments.should include("production")
-        @heroku.app_environments.should include("staging")
+        @config.app_environments.should have(2).environments
+        @config.app_environments.should include("production")
+        @config.app_environments.should include("staging")
       end
     end
 
     describe "#stack" do
       it "should return the associated stack for an environment" do
-        @heroku.stack("staging").should == "bamboo-ree-1.8.7"
+        @config.stack("staging").should == "bamboo-ree-1.8.7"
       end
 
       it "should default to the all setting if not explicitly defined" do
-        @heroku.stack("production").should == "bamboo-mri-1.9.2"
+        @config.stack("production").should == "bamboo-mri-1.9.2"
       end
     end
 
     describe "#config" do
       context "staging environment" do
         before(:each) do
-          @config = @heroku.config("staging")
+          @config = @config.config("staging")
         end
         it "should include configs defined in 'staging'" do
           @config["STAGING_CONFIG"].should == "special-staging"
@@ -66,7 +66,7 @@ module Heroku::Rails
     describe "#collaborators" do
       context "staging environment" do
         before(:each) do
-          @collaborators = @heroku.collaborators('staging')
+          @collaborators = @config.collaborators('staging')
         end
 
         it "should include the collaborators defined in 'all'" do
@@ -88,7 +88,7 @@ module Heroku::Rails
     describe "#domains" do
       context "staging environment" do
         before(:each) do
-          @domains = @heroku.domains('staging')
+          @domains = @config.domains('staging')
         end
 
         it "should include the domains defined in 'staging'" do
@@ -103,7 +103,7 @@ module Heroku::Rails
 
       context "production environment" do
         it "should include the domains defined in 'production'" do
-          @domains = @heroku.domains('production')
+          @domains = @config.domains('production')
           @domains.should include('awesomeapp.com')
           @domains.should include('www.awesomeapp.com')
         end
@@ -113,7 +113,7 @@ module Heroku::Rails
     describe "#addons" do
       context "staging environment" do
         before(:each) do
-          @addons = @heroku.addons('staging')
+          @addons = @config.addons('staging')
         end
 
         it "should include addons defined in 'all'" do
