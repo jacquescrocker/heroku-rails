95da8c100f6f511057b57db70842683b7a7b1824
Jacques Crocker <railsjedi@gmail.com>
2011-04-26 17:53:40 -0700

    Fixing railtie

--- lib/heroku-rails/railtie.rb
+++ lib/heroku-rails/railtie.rb
@@ -1,10 +1,8 @@
-module Heroku
-  module Rails
-    class Railtie < ::Rails::Railtie
-      rake_tasks do
-        HerokuRails::Config.root = ::Rails.root
-        load 'heroku/rails/tasks.rb'
-      end
+module HerokuRails
+  class Railtie < ::Rails::Railtie
+    rake_tasks do
+      HerokuRails::Config.root = ::Rails.root
+      load 'heroku/rails/tasks.rb'
     end
   end
 end
